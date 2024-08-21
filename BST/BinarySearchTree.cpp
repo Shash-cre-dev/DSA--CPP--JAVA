829d835695ff43604090ff40a0ff6ade2735e0ee
Shashank <shashwritescode@gmail.com>
2024-08-21 22:52:59 +0530

    Delete from BST

--- BST/BinarySearchTree.cpp
+++ BST/BinarySearchTree.cpp
@@ -80,6 +80,72 @@ void giveInput(Node *&root)
     }
 }
 
+Node *minVal(Node *root)
+{
+    Node *temp = root;
+    while (temp->left != NULL)
+    {
+        temp = temp->left;
+    }
+    return temp;
+}
+
+Node *deleteFromBST(Node *root, int val)
+{
+    // base case
+    if (root == NULL)
+    {
+        return root;
+    }
+
+    if (root->data == val)
+    {
+        // O child
+        if (root->left == NULL && root->right == NULL)
+        {
+            delete root;
+            return NULL;
+        }
+
+        // 1 child
+        // left child
+        if (root->left != NULL and root->right == NULL)
+        {
+            Node *temp = root->left;
+            delete root;
+            return temp;
+        }
+        // right child
+        if (root->left == NULL and root->right != NULL)
+        {
+            Node *temp = root->right;
+            delete root;
+            return temp;
+        }
+
+        // 2 child
+        if (root->left != NULL && root->right != NULL)
+        {
+            int mini = minVal(root->right)->data;
+            root->data = mini;
+            root->right = deleteFromBST(root->right, mini);
+            return root;
+        }
+    }
+
+    else if (root->data > val)
+    {
+        // left part
+        root->left = deleteFromBST(root->left, val);
+        return root;
+    }
+    else
+    {
+        // right part
+        root->right = deleteFromBST(root->right, val);
+        return root;
+    }
+}
 int main()
 {
     Node *root = NULL;
